c4fb0287ba0d6336bf5fb46a407e398b8f03a5c3
waldfe <kerwald.s@gmail.com>
2025-09-14 14:55:56 -0300

    adicionando codigo da maratona

---
+++ A-alimentacaoSaudavel.py
@@ -0,0 +1,25 @@
+
+def new_func():
+    
+    cont = 0
+    soma = 0
+
+    N, M = map( int, input().split() )
+
+    matrix = []
+
+    for i in range(N):
+        linha = list( map( int, input().split() ) )
+        matrix.append(linha)
+    
+
+    for j in range(M):
+        cont = 0
+        for i in range(N):
+            if matrix[i][j] > cont:
+                cont = matrix[i][j]
+        soma += cont
+        
+    print(soma)
+
+new_func()
